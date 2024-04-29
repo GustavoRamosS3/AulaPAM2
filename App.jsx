1a645bd9569e11341b64f022bee71095e1022ce8
GustavoRamosS3 <Guhpramos@gmail.com>
2024-04-29 14:41:03 -0300

    Projeto PAM2

---
+++ App.jsx
@@ -0,0 +1,40 @@
+import react from 'react';
+import { FlatList, View, Text, StyleSheet } from 'react-native';
+import Empresa from './Empresa';
+
+const dados = [
+  {Id:1, Nome: 'x', ValorDeMercado: '2999'},
+  {Id:2, Nome: 'a', ValorDeMercado: '2994'},
+  {Id:1, Nome: 'x', ValorDeMercado: '2594'}
+];
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Dados das empresas</Text>
+        <View style={styles.listaEmpresa}>
+            <FlatList
+              data={dados}
+              renderItem={({item})=>{
+                return(<Empresa 
+                  nome={item.Nome}
+                  valorMercado={item.ValorDeMercado}
+                />)
+              }}  
+            />
+        </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  listaEmpresa:{
+    height: 400
+  },
+});
